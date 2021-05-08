8cec2953a3630cb9226e5b042bdfcb500e8f0b76
Kornthip <sang.kornthip@gmail.com>
2021-05-08 01:11:40 -0400

    mvp are all functional

--- app.js
+++ app.js
@@ -3,8 +3,9 @@ const app = {};
 ///////////////get results from API and display as a list //////////////
 app.apiKey = `5b496702`;
 app.apiUrl = `http://www.omdbapi.com/`;
-app.resultsUl = document.querySelector('ul.resultsList');
 app.spanElement = document.querySelector('span.searchParameter');
+app.resultsUl = document.querySelector('ul.resultsList');
+app.nominationsList = document.querySelector('ul.nominationsList');
 
 //search for movie base on user input
 app.search = (userInput) => {
@@ -25,7 +26,7 @@ app.search = (userInput) => {
                 app.displayResults(movieResults,userInput)
             } else {
                 app.spanElement.textContent = `"${userInput}"`;
-                app.ulElement.innerHTML = `<h4>no results found</h4>`;
+                app.resultsUl.innerHTML = `<h4>no results found</h4>`;
             }
         })
 }
@@ -73,20 +74,63 @@ app.displayResults = (resultsArray,userInput) => {
         
     });
 }
-// function to listen to nominate button and save to nominations list
+
+// function to listen to nominate button and save to nominations list when user clicks on nominate button
 app.nominate = () => {
     app.resultsUl.addEventListener('click', function (e) {
-        const listIndex = e.target.id;
-        console.log(this.children[listIndex]);
+        if (e.target.tagName === "BUTTON") {
+            //select the clicked button
+            const button = e.target;
+            //title and year of nominated move
+            const nominateMovie = e.target.parentElement.children[0].textContent;
+            //user can nominate up to 5 movies, once hit 5 , alert that they at max
+            if (app.nominationsList.children.length < 5) {
+                //append to  nominations list
+                app.displayNominations(nominateMovie);
+                //disable the button if that movie is nominated
+                button.setAttribute("disabled",true)
+            } else {
+                alert("You have 5 nominations already");
+            } 
+        }
     });
 }
 
-//////////////get data from local storage and append to the nominations list ///////////////
+//display nominations
+app.displayNominations = (movie) => {
+    //create a list element
+    const nominatedMovie = document.createElement('li');
+    //create title of the movie
+    const movieTitle = document.createElement('p');
+    movieTitle.textContent = movie;
+
+    //create remove button element
+    const removeButton = document.createElement('button');
+    removeButton.textContent = 'remove';
+    //append title and button to li
+    nominatedMovie.appendChild(movieTitle);
+    nominatedMovie.appendChild(removeButton);
+    //append li to ul
+    app.nominationsList.appendChild(nominatedMovie);
+    if (app.nominationsList.children.length === 5) {
+        alert("Thank you for your nomination! You've reach the maximum of 5 nominations")
+    }
+}
+
+//function to listen to remove button to remove nominations when user click remove
+app.removeNomination = () => {
+    app.nominationsList.addEventListener('click', function (e) {
+        if (e.target.tagName === "BUTTON") {
+            //remove the nomination
+            this.removeChild(e.target.parentElement);
+        }
+    });
+}
 
 app.init = () => {
     app.checkInput();
     app.nominate();
-    console.log(localStorage);
+    app.removeNomination();
 }
 
 //initialize app
